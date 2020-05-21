dbbf827bfe462ccea5a1309322d91af998b54ea4
Tommaso Venza <tommasovenza@gmail.com>
2020-05-21 21:14:12 +0200

    update layout

--- js/script.js
+++ js/script.js
@@ -1,23 +1,34 @@
+// le variabili
 var buttonGenera = document.getElementById('form-genera');
 var buttonAnnulla = document.getElementById('form-annulla');
+var formKm = document.getElementById('form-km');
+var formEta = document.getElementById('form-eta');
 
 
 // evento quando si clicca su genera
 buttonGenera.addEventListener('click', 
     function() { 
+
 // gestione elaborazione biglietto
 var formNome = document.getElementById('form-nome').value;
-
-
+var formKm = document.getElementById('form-km').value;
+var formEta = document.getElementById('form-eta').value;
     }
 
 );
 
-//funzione annulla 
+
+//funzione Annulla 
 buttonAnnulla.addEventListener('click', 
     function() {
 // annulla tutte le variabili sottostanti attribuendo ad ognuna valore vuoto
 var formNome = document.getElementById('form-nome').value = '';
+var formKm = document.getElementById('form-km').value = '';
+// questa ha come valore il primo valore che si trova nella select
+var formEta = document.getElementById('form-eta').value = 'Minorenne';
 
 }  
+
 );
+
+
