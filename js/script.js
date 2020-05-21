2248f5e7aeae87ff359c9d5e846765c56fc44b7a
Tommaso Venza <tommasovenza@gmail.com>
2020-05-21 17:55:11 +0200

    aggiornamento layout

---
+++ js/script.js
@@ -0,0 +1,23 @@
+var buttonGenera = document.getElementById('form-genera');
+var buttonAnnulla = document.getElementById('form-annulla');
+
+
+// evento quando si clicca su genera
+buttonGenera.addEventListener('click', 
+    function() { 
+// gestione elaborazione biglietto
+var formNome = document.getElementById('form-nome').value;
+
+
+    }
+
+);
+
+//funzione annulla 
+buttonAnnulla.addEventListener('click', 
+    function() {
+// annulla tutte le variabili sottostanti attribuendo ad ognuna valore vuoto
+var formNome = document.getElementById('form-nome').value = '';
+
+}  
+);
