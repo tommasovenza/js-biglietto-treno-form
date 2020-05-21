09c33173fce38a710bc588ae6ea290e62b158370
Tommaso Venza <tommasovenza@gmail.com>
2020-05-21 23:59:37 +0200

    javascript update

--- js/script.js
+++ js/script.js
@@ -1,34 +1,62 @@
 // le variabili
 var buttonGenera = document.getElementById('form-genera');
 var buttonAnnulla = document.getElementById('form-annulla');
+
+// variabili del form
 var formKm = document.getElementById('form-km');
 var formEta = document.getElementById('form-eta');
+var formNome = document.getElementById('form-nome');
 
 
-// evento quando si clicca su genera
+//Elementi biglietto 
+var biglNome = document.getElementById('bigl-nome');
+var biglOfferta = document.getElementById('bigl-offerta');
+var biglCosto = document.getElementById('bigl-costo');
+
+// Evento quando si clicca su genera
 buttonGenera.addEventListener('click', 
     function() { 
 
-// gestione elaborazione biglietto
+// Gestione elaborazione biglietto (cosa succede quando clicco su genera dopo aver inserito i dati)
 var formNome = document.getElementById('form-nome').value;
 var formKm = document.getElementById('form-km').value;
 var formEta = document.getElementById('form-eta').value;
-    }
 
+// calcolo prezzo
+
+var prezzo = (formKm * 0.21);
+
+// sconti
+
+if ( formEta == 'Minorenne' ) {
+    prezzo = (prezzo - (prezzo * 20) / 100);
+} else if ( formEta == 'Anziano') {
+    prezzo = (prezzo - (prezzo * 40) / 100);
+}
+
+// Compilazione elementi biglietto
+biglNome.innerHTML = formNome;
+biglCosto.innerHTML = prezzo.toFixed(2) + " euro";
+ 
+    }
 );
 
-
-//funzione Annulla 
+//Funzione Annulla (ovvero cosa succede quando clicco sul tasto annulla)
 buttonAnnulla.addEventListener('click', 
     function() {
+
 // annulla tutte le variabili sottostanti attribuendo ad ognuna valore vuoto
+
 var formNome = document.getElementById('form-nome').value = '';
 var formKm = document.getElementById('form-km').value = '';
+
 // questa ha come valore il primo valore che si trova nella select
 var formEta = document.getElementById('form-eta').value = 'Minorenne';
 
-}  
+//gestione annullamento nome
+biglNome.innerHTML = '';
 
+    }  
 );
 
 
